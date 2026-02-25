ce6dcc21aba9491a0f51ea922c016117932fd8bd
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-02-25 13:40:36 +0800

    feat: add requirements, update settings, add base templates and use it in initial home page

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.getenv('STATIC_ROOT')
--- commongrounds/urls.py
+++ commongrounds/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path
+
+def home(request):
+    return render(request, 'home.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
