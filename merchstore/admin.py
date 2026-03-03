9ae6b797f27b38ca2b0e987134d44794f252a6eb
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-03-04 01:32:19 +0800

    feat: register models Product and ProductType to admin and do migrations

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, ProductType
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    model = ProductType
+
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+
+
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Product, ProductAdmin)
