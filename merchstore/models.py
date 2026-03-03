589cbe8af258e37717ebf8a3e75dd6cf5fdad2de
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-03-04 01:28:25 +0800

    feat: and product and product type to models

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    product_type = models.ForeignKey(
+        ProductType,
+        on_delete=models.SET_NULL,
+        null = True,
+        blank = True
+    )
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(self):
+        return reverse('merch:item-detail', args=[str(self.pk)])
