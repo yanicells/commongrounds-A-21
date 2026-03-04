60c5eabf03eb552f364a132f329cad7a796f85aa
PJ <edizon.luis.infante@student.ateneo.edu>
2026-03-05 01:39:07 +0800

    feat: register models Genre and Book in admin and do migrations

--- bookclub/admin.py
+++ bookclub/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Genre, Book
+
+
+class GenreAdmin(admin.ModelAdmin):
+    model = Genre
+
+
+class BookAdmin(admin.ModelAdmin):
+    model = Book
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Book, BookAdmin)
