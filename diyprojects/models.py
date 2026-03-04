819e7c569c2db1b6976ba598eaf3fa482b2ee8ca
Eostrixx <sofia.dion.torres@student.ateneo.edu>
2026-03-04 13:03:02 +0800

    feat: add project and project category to models

--- diyprojects/models.py
+++ diyprojects/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class ProjectCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        ProjectCategory,
+        on_delete=models.SET_NULL,
+        null = True,
+        blank = True
+    )
+    description = models.TextField()
+    materials = models.TextField()
+    steps = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(self):
+        return reverse("diyprojects:project-detail", args=[str(self.pk)])
+   
