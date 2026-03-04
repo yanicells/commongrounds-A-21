ae61d613e9e20216dd897e50af7896b8784a026c
Nathan Martin <gabryllmartin@gmail.com>
2026-03-04 19:01:21 +0800

    feat: created EventType and Event models

--- localevents/models.py
+++ localevents/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class EventType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        EventType, 
+        on_delete=models.SET_NULL, 
+        null=True,
+        )
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("localevents:event_detail", args=[str(self.pk)])
+    
+    
