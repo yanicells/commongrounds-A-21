7fd6244383c0a06484eafc3d08b4cf50a9d834be
PJ <edizon.luis.infante@student.ateneo.edu>
2026-03-05 01:34:07 +0800

    feat: add book and genre to models

--- bookclub/models.py
+++ bookclub/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    author = models.CharField()
+    publication_year = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publication_year']
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def get_absolute_url(self):
+        return reverse("book:book-detail", args=[str(self.pk)])
