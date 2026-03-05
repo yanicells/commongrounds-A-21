c50f1327d4c0b5a4315560d457299030265caa87
jktrr <jkterrobias@gmail.com>
2026-03-05 10:27:28 +0800

    fix: urls and missing model field

--- commissions/models.py
+++ commissions/models.py
@@ -11,6 +11,7 @@ class CommissionType(models.Model):
 class Commission(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    people_required = models.IntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
--- commissions/urls.py
+++ commissions/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.shortcuts import render
 from django.urls import path
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('')
+    path('requests', CommissionListView.as_view(), name='list_view'),
+    path('request/<int:pk>', CommissionDetailView.as_view(), name='detail_view')
 ]
