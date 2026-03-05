bfb180b24584539e1f393fef7c3771067826ad00
jktrr <jkterrobias@gmail.com>
2026-03-05 10:27:28 +0800

    fix: urls and missing model field

--- commissions/views.py
+++ commissions/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+class CommissionListView(ListView):
+    model = Commission
+    template_name = ''
+
+class CommissionDetailView(DetailView):
+    model = Commission
+    template_name = ''
