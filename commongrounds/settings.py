1afc239d81379200c1f8c1b36e67823fc3f1e8b2
jktrr <jkterrobias@gmail.com>
2026-03-05 12:22:30 +0800

    fix: removed key

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "pn_69i2r#^gtip8hq^bqssnov-ngdp9k-&f3qg7(d%e%43qui%" # os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
