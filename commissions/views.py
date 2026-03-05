117ff8f42078d70fef6e2f8323fa17ba821f3b38
jktrr <jkterrobias@gmail.com>
2026-03-05 12:20:57 +0800

    feat: detail view and fixed names

--- commissions/views.py
+++ commissions/views.py
@@ -5,8 +5,8 @@ from django.views.generic.detail import DetailView
 
 class CommissionListView(ListView):
     model = Commission
-    template_name = 'list.html'
+    template_name = 'commission_list.html'
 
 class CommissionDetailView(DetailView):
     model = Commission
-    template_name = 'commission.html'
+    template_name = 'commission_details.html'
