f6b025f221517c1cda9efb266c76dedfe5c00014
jktrr <jkterrobias@gmail.com>
2026-03-05 10:06:08 +0800

    feat: commission models

---
+++ commissions/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class CommissionType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on']
