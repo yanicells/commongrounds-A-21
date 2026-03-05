aa8bf0f9e9193548237f9ca58e9b9e87c5f7aead
jktrr <jkterrobias@gmail.com>
2026-03-05 12:00:04 +0800

    feat: list view

--- commissions/urls.py
+++ commissions/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('requests', CommissionListView.as_view(), name='list_view'),
     path('request/<int:pk>', CommissionDetailView.as_view(), name='detail_view')
 ]
+
+app_name = 'commissions'
