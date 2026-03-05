6171ac317a6eae81f072c27653a11c3b85a207e7
Edrian Miguel E. Capistrano <edrianmiguelcapistrano@gmail.com>
2026-03-06 01:06:02 +0800

    fix: follow pep8 for urls, update readme, remove setup guide

--- commongrounds/urls.py
+++ commongrounds/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.shortcuts import render
 from django.urls import include, path
 
+
 def home(request):
     return render(request, 'home.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
