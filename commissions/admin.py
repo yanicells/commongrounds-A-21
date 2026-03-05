e8f5e8c671257019eebad62512155a7d032adff4
jktrr <jkterrobias@gmail.com>
2026-03-05 10:39:52 +0800

    feat: class-based views and admin

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class CommissionTypeAdmin(admin.ModelAdmin):
+    model = CommissionType
+
+
+class CommissionTypeInline(admin.TabularInline):
+    model = CommissionType
+
+
+class CommissionAdmin(admin.ModelAdmin):
+    model = Commission
+    inlines = [CommissionTypeInline,]
+
+
+admin.site.register(Commission, CommissionAdmin)
+admin.site.register(CommissionType, CommissionTypeAdmin)
